6f12c2b5e73f96b29aff76c674ac6a52c530f6e6
Payneffff <1963221828@qq.com>
2021-09-15 22:10:37 +0800

    test:liupf commit test
    Signed-off-by: Payneffff <1963221828@qq.com>

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,7 @@
  * @Author: liupf
  * @Date: 2021-09-15 20:35:35
  * @LastEditors: liupf
- * @LastEditTime: 2021-09-15 21:18:55
+ * @LastEditTime: 2021-09-15 22:10:12
  */
 import React, { Component } from 'react'
 import './home.less'
@@ -14,6 +14,7 @@ class Home extends Component {
     return (
       <div className="P-home">
         <h1>Home page</h1>
+        <p>liupf test</p>
         <p>test</p>
       </div>
     )
