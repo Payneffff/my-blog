2ce90748f8e3d8f95cb1c0d5e09de5238580d7c0
liupf <payneffff@163.com>
2021-09-16 10:34:52 +0800

    增加一个简单的登录页
    Signed-off-by: liupf <payneffff@163.com>

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,32 +4,124 @@
  * @Author: liupf
  * @Date: 2021-09-15 20:35:30
  * @LastEditors: liupf
- * @LastEditTime: 2021-09-15 21:26:50
+ * @LastEditTime: 2021-09-16 10:22:51
  */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Button } from 'antd'
+import { Form, Input, Button, Checkbox, Card, message } from 'antd'
 import './login.less'
 
 class Login extends Component {
-  render() {
-    return (
-      <div className="P-login">
-        <h1>Login page</h1>
-        <Button type="primary" onClick={this.gotoHome.bind(this)}>
-          跳转Home页
-        </Button>
-      </div>
-    )
-  }
-
   gotoHome() {
     this.props.history.push('/home')
+  }
+
+  render() {
+    const formItemLayout = {
+      labelCol: {
+        xs: {
+          span: 24,
+        },
+        sm: {
+          span: 8,
+        },
+      },
+      wrapperCol: {
+        xs: {
+          span: 24,
+        },
+        sm: {
+          span: 16,
+        },
+      },
+    }
+
+    const onFinish = (values) => {
+      const { username, password } = values
+      if (username === 'admin' && password === 'admin') {
+        this.gotoHome()
+      } else {
+        message.error('密码错误')
+      }
+    }
+
+    const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo)
+    }
+
+    return (
+      <div className="P-login">
+        <Card>
+          <h1>Login page</h1>
+          <Form
+            name="basic"
+            {...formItemLayout}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="用户名"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入用户名',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="密码"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入密码',
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Checkbox>记住密码</Checkbox>
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+          {/* <Button type="primary" onClick={this.gotoHome.bind(this)}>
+          跳转Home页
+        </Button> */}
+        </Card>
+      </div>
+    )
   }
 }
 
 Login.propTypes = {
-  history: PropTypes.array,
+  history: PropTypes.object,
 }
 
 export default Login
